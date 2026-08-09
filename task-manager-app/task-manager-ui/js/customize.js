dc278c6a148b2f5c7d7ff667d929914a2931a17b
JayceFuller <jaycefuller05@protonmail.com>
2026-08-09 17:20:52 -0400

    Start of customize page

---
+++ task-manager-app/task-manager-ui/js/customize.js
@@ -0,0 +1,9 @@
+//Previous colors
+const backgroundColor = localStorage.getItem('backgroundColor');
+const accentColor = localStorage.getItem('accentColor');
+const fontColor = localStorage.getItem('fontColor');
+
+//Form fields
+const backgroundPicker = document.getElementById('background-picker');
+const accentPicker = document.getElementById('accent-picker');
+const fontColorPicker = document.getElementById('font-color-picker');
